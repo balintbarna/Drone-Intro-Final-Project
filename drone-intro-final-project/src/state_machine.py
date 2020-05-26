b5269221ff0d4702d7f2e7f018e28a9f3fffb249
Balint Kovari <balintmaci@gmail.com>
2020-05-26 05:27:19 -0700

    added close pose error state

--- drone-intro-final-project/src/state_machine.py
+++ drone-intro-final-project/src/state_machine.py
@@ -6,18 +6,22 @@ from geometry_msgs.msg import TwistStamped, PoseStamped, PoseWithCovarianceStamp
 import rospy
 from simple_pid import PID
 
-from message_tools import orientation_to_yaw, point_to_arr, arr_to_point
+from message_tools import orientation_to_yaw, yaw_to_orientation, point_to_arr, arr_to_point, create_setpoint_message_pos_yaw
 
 class StateMachine():
     class States(Enum):
         IDLE = "idle"
         WAITING_TO_ARRIVE = "wait_arrive"
         TAKE_OFF = "takeoff"
+        CLOSE_POSE_ERROR = "pose_error"
 
     def __init__(self):
         self._current_value = self.States.IDLE
         self._next_value = self.States.IDLE
         self.pose_error = Pose()
+
+        self.max_angle_error = 0.1
+        self.max_pose_error = 0.1
     
     def _pose_error_callback(self, topic = PoseStamped()):
         self.pose_error = topic.pose
@@ -46,8 +50,26 @@ class StateMachine():
             self._mav1.set_target_pos(target)
             self.set_current_state(self.States.WAITING_TO_ARRIVE)
             self.set_next_state(self.States.IDLE)
-
-
-
-
-
+        
+        elif cur == self.States.CLOSE_POSE_ERROR:
+            if self.pose_error == None:
+                return
+            pose_error_arr = point_to_arr(self.pose_error.position)
+            angle_error = orientation_to_yaw(self.pose_error.orientation)
+            self.pose_error = None
+            magnitude = np.linalg.norm(pose_error_arr)
+            # check if limit is reached
+            if abs(angle_error) < self.max_angle_error and magnitude < self.max_pose_error:
+                self.set_current_state(self.States.IDLE)
+                print("ALIGNED")
+                return
+            # calc angle
+            current_yaw = orientation_to_yaw(self._mav1.current_pose.pose.orientation)
+            target_yaw = current_yaw + angle_error
+            # calc pos
+            current_pos = point_to_arr(self._mav1.current_pose.pose.position)
+            target_pos = current_pos + pose_error_arr
+            target_point = arr_to_point(target_pos)
+            # send
+            target_pose = create_setpoint_message_pos_yaw(target_point, target_yaw)
+            self._mav1.set_target_pose(target_pose)
